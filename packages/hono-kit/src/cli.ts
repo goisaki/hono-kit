500a7b876da2b09204cf4878146b657f173d53ce
Kengo Watanabe <ken5@kwtc.jp>
2024-05-27 16:39:14 +0900

    feat: update npm packages structure

---
+++ packages/hono-kit/src/cli.ts
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+import { platform, arch } from "process";
+import {} from "node:fs";
+
+const targets: { target: string; url: string }[] = [
+  {
+    target: "darwin-x64",
+    url: "${darwin-x64-url}",
+  },
+];
+const main = async (): Promise<void> => {
+  console.log("hono-kit");
+  const target = `${platform}-${arch}`;
+  if (!targets.find((t) => t.target === target)) {
+    throw new Error(`unsupported target: ${target}`);
+  }
+};
+
+main();
